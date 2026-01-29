202527ff2f0be6298eb06378e9446abcdf548573
Ian M. Melsom <ian@melsom.be>
2026-01-29 15:21:55 +0100

    post install comitt

--- users/init.php
+++ users/init.php
@@ -35,3 +35,60 @@ $GLOBALS['config'] = array(
 	'mysql'      => array(
 	'force_utc_mysql' => false,
 	'charset'      => 'utf8mb4',		
+'host'         => 'localhost',
+'username'     => 'vaktplan',
+'password'     => '8fafqJ92_Uztp+8rR5',
+'db'           => 'vaktplanv3_users',
+'port'         => '3306',
+),
+'remember'        => array(
+  'cookie_name'   => 'hIenkKNvEGsnNXk1fAdf',
+  'cookie_expiry' => 604800  //One week, feel free to make it longer
+),
+'session' => array(
+  'session_name' => 'w5n6kAmkC3cIuxCdmQZF',
+  'token_name' => 'token',
+)
+);
+
+//If you changed your UserSpice or UserCake database prefix
+//put it here.
+$db_table_prefix = "uc_";  //Old database prefix
+
+//adding more ids to this array allows people to access everything, whether offline or not. Use caution.
+$master_account = [1];
+
+$currentPage = currentPage();
+
+//Check to see if user has a remember me cookie
+if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))){
+	$hash = Cookie::get(Config::get('remember/cookie_name'));
+	$hashCheck = DB::getInstance()->query("SELECT * FROM users_session WHERE hash = ? AND uagent = ?",array($hash,Session::uagent_no_version()));
+
+	if ($hashCheck->count()) {
+		$user = new User($hashCheck->first()->user_id);
+		$inst = Config::get('session/session_name');
+        $_SESSION[$inst . '_login_method'] = "cookie";
+        $user->login();
+
+	}
+}
+
+//Check to see that user is logged in on a temporary password
+$user = new User();
+
+//Check to see that user is verified
+if($user->isLoggedIn()){
+	if($user->data()->email_verified == 0 && $currentPage != 'verify.php' && $currentPage != 'logout.php' && $currentPage != 'verify_thankyou.php'){
+		Redirect::to($us_url_root.'users/verify.php');
+	}
+}
+
+
+$usespice_nonce = base64_encode(random_bytes(16));
+// Forces SSL verification in cURL requests to UserSpice API
+// Will most likely break on localhost or self-signed certificates
+define('EXTRA_CURL_SECURITY', false); 
+require_once $abs_us_root.$us_url_root."users/includes/loader.php";
+$timezone_string = 'Europe/Oslo';
+date_default_timezone_set($timezone_string);
