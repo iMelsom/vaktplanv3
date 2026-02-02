c56f4c0e773d0b76e755cda84405e2c3e5ab5b90
ian <ian@Idoru>
2026-02-02 13:05:38 +0100

    trfgdb

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-if(file_exists("install/index.php")){
-	//perform redirect if installer files exist
-	//this if{} block may be deleted once installed
-	header("Location: install/index.php");
-}
 
 require_once 'users/init.php';
 require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
