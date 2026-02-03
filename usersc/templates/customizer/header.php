5891091ea04e5940464915b41ae2de1236298b90
ian <ian@Idoru>
2026-02-03 10:27:39 +0100

    aerdfs

--- usersc/templates/customizer/header.php
+++ usersc/templates/customizer/header.php
@@ -55,7 +55,41 @@ if(isset($child_theme) && $child_theme != ''){
 if(file_exists($abs_us_root.$us_url_root.'usersc/templates/'.$settings->template.'.css')){?>
   <link href="<?=$us_url_root?>usersc/templates/<?=$settings->template?>.css" rel="stylesheet">
 <?php } ?>
+<?php  //Vaktplan-additions:
+if (!isset($urlmod)) $urlmod ="";
+if(file_exists($abs_us_root.$us_url_root.'/vaktplan_files/css/vaktplan.css')){?><link href="/vaktplan_files/css/<?php echo $urlmod;?>vaktplan.css" rel="stylesheet">
+<?php } ?>
 
+
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/css/spectrum.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/js/jquery-ui/jquery-ui.min.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/js/jquery-ui/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/js/jquery-ui/jquery-ui.structure.min.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/js/jquery-ui/jquery-ui.structure.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/js/jquery-ui/jquery-ui.theme.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/js/jquery-ui/jquery-ui.theme.min.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/js/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="/vaktplan_files/css/timepicker.css">
+
+
+<script src = "/vaktplan_files/js/jquery-3.4.1.min.js">  </script>
+<script src = "/vaktplan_files/js/jquery-ui/jquery-ui.min.js">  </script>
+<script src = "/vaktplan_files/js/jquery-ui/jquery-ui.js">  </script>
+<script src = "/vaktplan_files/js/daterangepicker/moment.min.js">  </script>
+<script src = "/vaktplan_files/js/daterangepicker/daterangepicker.js">  </script>
+<script src = "/vaktplan_files/js/editDialog_frav.js">  </script>
+<script src = "/vaktplan_files/js/editDialog_vakt.js">  </script>
+<script src = "/vaktplan_files/js/editDialog_turadm.js">  </script>
+<script src = "/vaktplan_files/js/T_mineinstillinger_autosaver.js">  </script>
+
+<script type="text/javascript" src="/vaktplan_files/js/spectrum.js"></script>
+<script src = "/vaktplan_files/js/colorpicker.js">  </script>
+<script src = "/vaktplan_files/js/timepicker.js">  </script>
+<script src = "/vaktplan_files/js/datepicker-nn.js">  </script>
+<script src = "/vaktplan_files/js/datepicker-nb.js">  </script>
+<script src=  "/vaktplan_files/js/jscolor/jscolor.js"></script>
+
+<?php //vaktplan-addition ends ?>
 </head>
 <body class="d-flex flex-column min-vh-100">
 <?php require_once($abs_us_root.$us_url_root.'users/includes/template/header3_must_include.php'); ?>
