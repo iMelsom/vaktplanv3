947a1631e7bec6ca70a9e33b4db645172b92355a
ian <ian@Idoru>
2026-02-04 08:57:04 +0100

    aerdfs

--- users/init.php
+++ users/init.php
@@ -29,11 +29,10 @@ for($i = 1; $i < $self_path_length; $i++){
 }
 
 require_once $abs_us_root.$us_url_root.'users/helpers/helpers.php';
-require '/var/www/html/underutvikling/vaktplanv3/db_info.php';
+require getcwd().'/db_info.php';
 $username = $config_mysql_user;
 $pass = $config_mysql_pass;
 $InitBase= 'vaktplanv3_users';
-error_log(getcwd() );
 
 // Set config
 $GLOBALS['config'] = array(
